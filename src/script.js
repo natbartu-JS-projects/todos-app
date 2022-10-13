c9069e9357db9c87de2639e15b2a6d3221a60aa5
natacha <nat_bartu@hotmail.com>
2022-10-13 11:08:19 +1000

    add event listener into the form and add build in function prevent Default

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
