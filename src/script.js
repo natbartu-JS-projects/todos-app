1ddb7dea084d36db0b8a9bc6fcf2a88e7831ffb6
natacha <nat_bartu@hotmail.com>
2022-10-13 11:33:42 +1000

    add local storage functionality

--- src/script.js
+++ src/script.js
@@ -2,6 +2,11 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUl = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -23,17 +28,36 @@ function addTodo(todo) {
     }
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLocalStorage();
+    });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
+      updateLocalStorage();
     });
 
     todosUl.appendChild(todoEl);
 
     input.value = "";
+
+    updateLocalStorage();
   }
+}
+
+function updateLocalStorage() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
